91a55dfe78944dee24e22ae606c1ad748df19a54
MananGadhiyaSureshbhai <17ce029@charusat.edu.in>
2020-06-23 12:54:12 +0530

    second commit

--- App.js
+++ App.js
@@ -5,26 +5,62 @@ import {
   View,
   Image,
   TouchableOpacity,
-  TouchableHighlight,
+  Slider,
 } from "react-native";
 import { Audio } from "expo-av";
 import { Ionicons } from "@expo/vector-icons";
 import { AntDesign, MaterialIcons, Octicons } from "@expo/vector-icons";
-//import { Audio } from "expo-av";
+
 const soundObject = new Audio.Sound();
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isPlaying: false,
-      pause: "false",
-      image: "./play.png",
-      name: "play",
+      position: 0,
+      timeLeft: "",
       ismute: false,
+      remaintime: "",
+      positiontime: "",
+      durationtime: "",
+      isdurationorposition: true,
     };
-  }
-  componentDidMount() {
     soundObject.loadAsync(require("./hindi.mp3"));
+  }
+
+  getStatus = async () => {
+    var status = await soundObject.getStatusAsync();
+    var percentage =
+      (status["positionMillis"] / status["durationMillis"]) * 1000;
+    var remainingTime = status["durationMillis"] - status["positionMillis"];
+    var remainminute = remainingTime / 1000 / 60;
+    var remainsecond = (remainingTime / 1000) % 60;
+    var positionminute = status["positionMillis"] / 1000 / 60;
+    var positionsecond = (status["positionMillis"] / 1000) % 60;
+    var durationminute = status["durationMillis"] / 1000 / 60;
+    var durationsecond = (status["durationMillis"] / 1000) % 60;
+    this.setState({
+      position: percentage,
+      timeLeft: remainingTime,
+      remaintime:
+        remainminute.toString().split(".")[0] +
+        ":" +
+        remainsecond.toString().split(".")[0],
+      positiontime:
+        positionminute.toString().split(".")[0] +
+        ":" +
+        positionsecond.toString().split(".")[0],
+      durationtime:
+        durationminute.toString().split(".")[0] +
+        ":" +
+        durationsecond.toString().split(".")[0],
+    });
+  };
+  componentDidMount() {
+    this.interval = setInterval(() => this.getStatus(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
   handlePlayPause = async () => {
     const { isPlaying } = this.state;
@@ -41,6 +77,11 @@ export default class App extends React.Component {
       isPlaying: true,
     });
   };
+  durationposition = async () => {
+    this.setState({
+      isdurationorposition: !this.state.isdurationorposition,
+    });
+  };
   MuteUnmute = async () => {
     const { ismute } = this.state;
     ismute
@@ -51,41 +92,101 @@ export default class App extends React.Component {
       ismute: !ismute,
     });
   };
+  onValueChange = async (value) => {
+    var status = await soundObject.getStatusAsync();
+    var p = (value / 1000) * status["durationMillis"];
+    soundObject.setStatusAsync({ positionMillis: p });
+    var percentage = (p / status["durationMillis"]) * 1000;
+    this.setState({
+      position: percentage,
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity style={styles.control} onPress={this.handlePlayPause}>
-          {this.state.isPlaying ? (
-            <Ionicons name="ios-pause" size={48} color="#444" />
-          ) : (
-            <Ionicons name="ios-play-circle" size={48} color="#444" />
-          )}
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {
-            this.Replay();
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          <MaterialIcons
-            name="replay"
-            size={48}
-            //color="black"
-            style={styles.control}
-          />
-        </TouchableOpacity>
+          <Text style={{ color: "black" }}>{this.state.positiontime}</Text>
 
-        <TouchableOpacity
-          style={styles.control}
-          onPress={() => {
-            this.MuteUnmute();
+          <Slider
+            minimumValue={1}
+            maximumValue={1000}
+            minimumTrackTintColor="#1EB1FC"
+            maximumTractTintColor="#1EB1FC"
+            step={1}
+            value={this.state.position}
+            onValueChange={(value) => this.onValueChange(value)}
+            style={styles.slider}
+            thumbTintColor="#1EB1FC"
+          />
+          <TouchableOpacity onPress={this.durationposition}>
+            {this.state.isdurationorposition ? (
+              <Text
+                style={{
+                  color: "black",
+                }}
+              >
+                {this.state.durationtime}
+              </Text>
+            ) : (
+              <Text
+                style={{
+                  color: "black",
+                }}
+              >
+                -{this.state.remaintime}
+              </Text>
+            )}
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          {this.state.ismute ? (
-            <Octicons name="unmute" size={48} color="#444" />
-          ) : (
-            <Octicons name="mute" size={48} color="#444" />
-          )}
-        </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              this.Replay();
+            }}
+          >
+            <MaterialIcons
+              name="replay"
+              size={48}
+              //color="black"
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={this.handlePlayPause}
+            style={{ marginLeft: 70, marginRight: 70 }}
+          >
+            {this.state.isPlaying ? (
+              <Ionicons name="ios-pause" size={48} color="#444" />
+            ) : (
+              <Ionicons name="ios-play-circle" size={48} color="#444" />
+            )}
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              this.MuteUnmute();
+            }}
+          >
+            {this.state.ismute ? (
+              <Octicons name="unmute" size={48} color="#444" />
+            ) : (
+              <Octicons name="mute" size={48} color="#444" />
+            )}
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -97,9 +198,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    flexDirection: "row",
+  },
+  slider: {
+    width: 300,
   },
   control: {
-    margin: 20,
+    margin: 2,
   },
 });
